96832223c001d8b4fc7b4ed473148c02a9ebca5e
pschild <philippe.schild@gmail.com>
2018-02-27 13:37:52 +0100

    styling

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,7 @@
 import React from "react";
+import Age from "../components/Age/Age";
 
-import FaXing from 'react-icons/lib/fa/xing';
-import FaLinkedin from 'react-icons/lib/fa/linkedin';
-import FaGithub from 'react-icons/lib/fa/github';
-import FaEnvelopeO from 'react-icons/lib/fa/envelope-o';
+import {FaXing, FaLinkedin, FaGithub, FaEnvelopeO} from 'react-icons/lib/fa';
 
 export default () => (
     <div>
@@ -16,7 +14,7 @@ export default () => (
             </div>
             <div className="item" style={{flex: 2}}>
                 <div>Philippe Schild</div>
-                <div>28 Jahre</div>
+                <div><Age/> Jahre</div>
                 <div>Fachinformatiker (Anwendungsentwicklung)</div>
                 <div>B.Sc.</div>
                 <div>Wirtschaftsinformatik (M.Sc.), Hochschule Niederrhein</div>
@@ -25,17 +23,29 @@ export default () => (
 
         <h3>Kontakt</h3>
         <div style={{display: 'flex', alignItems: 'center'}}>
-            <div style={{flex: 1}}>
-                <FaEnvelopeO/>
+            <div style={{flex: 1, textAlign: 'center'}}>
+                <a href="mailto:philippe@pschild.de">
+                    <FaEnvelopeO size={40}/>
+                    <div>philippe@pschild.de</div>
+                </a>
             </div>
-            <div style={{flex: 1}}>
-                <FaXing/>
+            <div style={{flex: 2, textAlign: 'center'}}>
+                <a href="https://www.xing.com/profile/Philippe_Schild" target="_blank">
+                    <FaXing size={40}/>
+                    <div>Philippe Schild</div>
+                </a>
             </div>
-            <div style={{flex: 1}}>
-                <FaLinkedin/>
+            <div style={{flex: 2, textAlign: 'center'}}>
+                <a href="https://de.linkedin.com/pub/philippe-schild/108/393/754" target="_blank">
+                    <FaLinkedin size={40}/>
+                    <div>Philippe Schild</div>
+                </a>
             </div>
-            <div style={{flex: 1}}>
-                <FaGithub/>
+            <div style={{flex: 1, textAlign: 'center'}}>
+                <a href="https://github.com/pschild" target="_blank">
+                    <FaGithub size={40}/>
+                    <div>pschild</div>
+                </a>
             </div>
         </div>
 
