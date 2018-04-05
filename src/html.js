e73eb43824a7966e9a04fcf1333a7c0cff6680fb
pschild <philippe.schild@gmail.com>
2018-04-05 17:40:43 +0200

    changed color for mobile browser

--- src/html.js
+++ src/html.js
@@ -28,7 +28,7 @@ module.exports = class HTML extends React.Component {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
                 <meta name="apple-mobile-web-app-capable" content="yes"/>
                 <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
-                <meta name="theme-color" content="#227cc0"/>
+                <meta name="theme-color" content="#3498DB"/>
                 {this.props.headComponents}
                 <link rel="apple-touch-icon" href={appleTouchIcon}/>
                 <link rel="apple-touch-icon-precomposed" href={appleTouchIconPrecomposed}/>
