cc8717739a82d6b4d3a8f813c9baf6bea164abec
pschild <philippe.schild@gmail.com>
2018-04-10 22:51:52 +0200

    added lang attribute to html tag

--- src/html.js
+++ src/html.js
@@ -21,7 +21,7 @@ module.exports = class HTML extends React.Component {
             )
         }
         return (
-            <html {...this.props.htmlAttributes}>
+            <html lang="de" {...this.props.htmlAttributes}>
             <head>
                 <meta charSet="utf-8"/>
                 <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
